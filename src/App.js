e4856d0dfbcb6f6caff0c06aab769df410ccfaf4
harshita <harshita1225@gmail.com>
2023-01-05 13:12:14 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Scoreboard from "./components/Scoreboard";
+import { Route, Routes } from "react-router-dom";
+import ScoreContextProvider from "./Contexts/ScoreContext";
+import InfoBoard from "./components/InfoBoard";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Scoreboard />}></Route>
+      <Route path="/infoboard" element={<InfoBoard />}></Route>
+      <Route path="*" element={<Scoreboard />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
